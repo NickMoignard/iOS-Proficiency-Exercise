c4736c7c020d958bde1c59402296badf072c4000
nickmoignard <nicklmoignard@gmail.com>
2018-04-18 15:54:30 +1000

    working on dynamic cell height. Will complete from home

---
+++ iOS Proficiency Exercise/Controllers/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  iOS Proficiency Exercise
+//
+//  Created by Nick Moignard on 18/4/18.
+//  Copyright © 2018 Nick Moignard. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
